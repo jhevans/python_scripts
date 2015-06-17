9fa7af007af174042eb80bc74a7e725c2005efba
JohnH.Evans <johnh.evans@autotrader.co.uk>
2015-06-17 11:56:39 +0100

    fixed typo to protect myself from the grammar Nazis

--- int_demo.py
+++ int_demo.py
@@ -1,11 +1,17 @@
-# An demonstration of Python's differing behaviour when naming small and large ints
+# A demonstration of Python's differing behaviour when naming small and large ints
 
 low_int_1 = 1
 low_int_2 = 0 + 1
-
+print("low int equals:")
+print(low_int_1 == low_int_2)
+print("low int is:")
 print(low_int_1 is low_int_2)
+print("\n")
 
 high_int_1 = 12345
 high_int_2 = 12344 + 1
 
+print("high int equals:")
+print(high_int_1 == high_int_2)
+print("high int is:")
 print(high_int_1 is high_int_2)
